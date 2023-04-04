5519b0f8046e9e6b08c0c96e1356c36871f5a350
Allen Vincent Gonzales <vincegonzalesdlsu@gmail.com>
2023-04-04 16:02:06 +0800

    Restructure folder

--- src/@types/httpOptions.ts
+++ src/@types/httpOptions.ts
@@ -8,27 +8,3 @@ export type Header = {
   'Content-Type'?: string;
   Authorization?: string;
 };
-
-export type GetWeatherParams = {
-  countryCode: string;
-  postalCode: string;
-};
-
-export type WeatherResponse = {
-  lon: number;
-  lat: number;
-  main: string;
-  description: string;
-  temp: number;
-  feels_like: number;
-  temp_min: number;
-  temp_max: number;
-  pressure: number;
-  humidity: number;
-};
-
-export type Response = {
-  code: number;
-  message: string;
-  weather?: WeatherResponse | null;
-};
--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,1 +1,2 @@
 export * from './httpOptions';
+export * from './customTypes';
